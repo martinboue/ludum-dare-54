f1b882d27da57e1d96256a57a02206f5c4c07b3d
Thomas <epis.ajout@gmail.com>
2023-10-01 22:20:13 +0200

    feat: various enemy types

--- src/map/spawner/spawner.gd
+++ src/map/spawner/spawner.gd
@@ -1,12 +1,21 @@
 # Spawner
 extends Marker2D
 
-var enemy_scene = preload("res://src/units/enemies/enemy.tscn")
+var enemy_scenes = [
+	preload("res://src/units/enemies/archer/enemy_archer.tscn"),
+	preload("res://src/units/enemies/soldier/enemy.tscn"),
+	preload("res://src/units/enemies/shielder/enemy_shielder.tscn")
+]
 
-
+func _ready() -> void:
+	randomize()
 
 func _on_spawn_cooldown_timeout() -> void:
-	var enemy = enemy_scene.instantiate()
+	var enemy = _get_random_enemy().instantiate()
 	get_parent().add_child(enemy)
 	enemy.position = position
 	ScoreManager.on_enemy_spawn(enemy)
+	
+func _get_random_enemy() -> PackedScene:
+	# Todo weight and enemies pattern
+	return enemy_scenes.pick_random()
---
+++ src/units/enemies/soldier/enemy.gd
@@ -0,0 +1,2 @@
+class_name EnemySoldier
+extends Unit
