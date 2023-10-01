00e44e86f9581129ae3e4937db2dedd6f3f9067a
Thomas <epis.ajout@gmail.com>
2023-10-01 15:37:54 +0200

    fix: unit picker selection

--- src/hud.gd
+++ src/hud.gd
@@ -4,11 +4,12 @@ signal unit_selection_changed(selected_unit)
 signal restart()
 
 var selected_unit = null
+var unit_pickers = []
 
 func _ready() -> void:
 	$Defeat.visible = false
-	
-	for c in get_tree().get_nodes_in_group("unit_picker"):
+	unit_pickers= get_tree().get_nodes_in_group("unit_picker")
+	for c in unit_pickers:
 		if c is UnitPicker:
 			c.selection_changed.connect(_raise_unit_selection_changed)
 
@@ -16,6 +17,10 @@ func _raise_unit_selection_changed(is_selected, value) -> void:
 	if is_selected:
 		selected_unit = value
 		unit_selection_changed.emit(selected_unit)
+		# Unselect other unit picker
+		for u in unit_pickers:
+			if u.value != value:
+				u.unselect()
 	elif selected_unit == value:
 		selected_unit = null
 		unit_selection_changed.emit(selected_unit)
