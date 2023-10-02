eb0a8a3af152300d99b70991665ae1ae0359eb64
Martin Boué <martbou35@gmail.com>
2023-10-02 15:15:36 +0200

    Rollback archer attack

--- src/units/enemies/archer/enemy_archer.gd
+++ src/units/enemies/archer/enemy_archer.gd
@@ -4,15 +4,17 @@ extends Unit
 var arrow_scene = preload("res://src/units/enemies/archer/arrow/arrow.tscn")
 
 func shoot():
-	$ShootCooldown.start()
 	var arrow = arrow_scene.instantiate()
 	get_parent().add_child(arrow)
 	arrow.global_position = $ArrowSpawner.global_position
 
 func _on_shot_cooldown_timeout() -> void:
-	if enemies_in_range > 0:
-		shoot()
+	shoot()
 
 func _on_archer_at_range() -> void:
 	if $ShootCooldown.is_stopped():
-		shoot()
+		$ShootCooldown.start()
+		
+func _on_archer_not_at_range() -> void:
+	if not $ShootCooldown.is_stopped():
+		$ShootCooldown.stop()
